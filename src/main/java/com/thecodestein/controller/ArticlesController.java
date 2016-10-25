c86ac94187a1584d5d28e71d13f04f7c70342bfe
Ricardo Villanueva <villanueva.ricardo@gmail.com>
2016-10-25 23:11:44 +0100

    using context path received in request

--- src/main/java/com/thecodestein/controller/ArticlesController.java
+++ src/main/java/com/thecodestein/controller/ArticlesController.java
@@ -1,11 +1,13 @@
 package com.thecodestein.controller;
 
 import com.thecodestein.service.FileService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Set;
@@ -16,11 +18,10 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 /**
  * Provide the mechanisms to interact with the existent articles shared in the static folder
  */
+@Slf4j
 @RestController
 @RequestMapping(value = "/api")
 public class ArticlesController {
-
-    private final String contextUrl;
 
     private final String articlesRelativePath;
 
@@ -31,20 +32,42 @@ public class ArticlesController {
     @Autowired
     public ArticlesController(FileService fileService,
                               @Value("${thecodestein.articlesFullPath}") String articlesPath,
-                              @Value("${thecodestein.contextUrl}") String contextUrl,
                               @Value("${thecodestein.articlesRelativePath}") String articlesRelativePath) {
         this.fileService = fileService;
         this.articlesPath = articlesPath;
-        this.contextUrl = contextUrl;
         this.articlesRelativePath = articlesRelativePath;
     }
 
     @RequestMapping(value = "/articles", method = GET)
-    public Set<String> getArticles() {
+    public Set<String> getArticles(HttpServletRequest request) {
         Path path = Paths.get(articlesPath);
+        String contextUrl = getContextPath(request);
         return fileService.getFiles(path.toFile())
                 .stream()
                 .map(fileName -> contextUrl + articlesRelativePath + fileName)
                 .collect(Collectors.toSet());
+    }
+
+    private String getContextPath(HttpServletRequest request) {
+
+        String port;
+
+        if (request.getServerPort() == 80) {
+            port = "";
+        }
+        else if (request.getServerPort() == 443) {
+            port = "";
+        }
+        else {
+            port = ":" + request.getServerPort();
+        }
+
+        String template = "{scheme}://{serverName}{port}/";
+        template = template.replace("{scheme}", request.getScheme());
+        template = template.replace("{serverName}", request.getServerName());
+        template = template.replace("{port}", port);
+
+        log.debug("Context Path resolved '{}'", template);
+        return template;
     }
 }
